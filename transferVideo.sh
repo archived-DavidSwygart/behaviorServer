9943d8ed38368136257112bb14354273cc7edf69
davidswygart <davidswygart@gmail.com>
2024-09-13 19:14:30 -0400

    Update transferVideo.sh

--- transferVideo.sh
+++ transferVideo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #not working, @ sign digests next few characters (10)
 
-cssh piCluster -a "sh cam/transferVideo.sh lapishla@10.1.1.0:/home/lapishla/Desktop/testCopy"
+cssh piCluster -a "sh cam/transferVideo.sh server /home/server/Desktop/allData"
