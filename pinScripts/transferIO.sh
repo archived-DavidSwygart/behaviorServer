47809bab9e0280258f98990f808cc7736372409c
davidswygart <davidswygart@gmail.com>
2024-09-26 12:07:17 -0400

    Update transferIO.sh

--- pinScripts/transferIO.sh
+++ pinScripts/transferIO.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "transferring pins recording"
-cssh piCluster -a "sh pins/transferIO.sh server /home/server/Desktop/allData"
+cssh piCluster -a "sh pins/transferIO.sh server /home/server/Desktop/piData"
