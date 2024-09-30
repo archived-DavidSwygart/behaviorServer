1797f73b627e9126f0eff8224e980be91e61e0fe
davidswygart <davidswygart@gmail.com>
2024-09-30 11:42:15 -0400

    stop experiment with single cssh

--- stopExperiment.sh
+++ stopExperiment.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 cd $(dirname "$(readlink -f "$0")")
 
-echo "stopping video"
-sh cameraScripts/stopVideo.sh
-
-echo "stopping audio"
-sh microphoneScripts/stopAudio.sh
-
-echo "stopping pins"
-sh pinScripts/stopInput.sh
+cssh piCluster -a "bash stopExperiment.sh"
 
 #echo "transferring data"
 #sh transferData.sh
