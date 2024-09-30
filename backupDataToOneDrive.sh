5e0773543f3f451555e00407ebb2e8403d52415f
davidswygart <davidswygart@gmail.com>
2024-09-30 11:18:22 -0400

    rclone doesn't need to hash already copied files

--- backupDataToOneDrive.sh
+++ backupDataToOneDrive.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Backup the piData folder to David's Onerive
-xfce4-terminal --command='rclone copy /home/server/Desktop/piData OneDrive_David:/piData_backup --checksum --metadata --no-update-modtime -v'
+echo "Uploading the piData folder to David's Onerive"
+xfce4-terminal --hold --command='rclone copy /home/server/Desktop/piData OneDrive_David:/piData_backup --metadata --human-readable -v'
