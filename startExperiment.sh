4a36a365984c22c61d968c2300bd5b0c8cc2a2c1
davidswygart <davidswygart@gmail.com>
2024-09-30 11:42:38 -0400

    auto fill default start of session

--- startExperiment.sh
+++ startExperiment.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd $(dirname "$(readlink -f "$0")")
+
+day="$(date +%F)_"
+
 echo 'Enter session name:'
-read session
+read -e -i "$day" session
 
 echo "experiment started at $(date)"
 cssh piCluster -a "sh startExperiment.sh $session"
-
-echo "camera, microphone, and pins started. Hit any button to close window"
-read
