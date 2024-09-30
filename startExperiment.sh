d65dbf0aac795a9c05dfe28c07374e7619e5b4b6
davidswygart <davidswygart@gmail.com>
2024-09-30 11:18:57 -0400

    run 1 cssh to start experiment

--- startExperiment.sh
+++ startExperiment.sh
@@ -4,9 +4,7 @@ echo 'Enter session name:'
 read session
 
 echo "experiment started at $(date)"
-sh pinScripts/recordInput.sh $session
-sh microphoneScripts/recordAudio.sh $session
-sh cameraScripts/recordVideo.sh $session
+cssh piCluster -a "sh startExperiment.sh $session"
 
 echo "camera, microphone, and pins started. Hit any button to close window"
 read
