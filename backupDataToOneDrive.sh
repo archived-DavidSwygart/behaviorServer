7fec2012fa757d070fb8f086731a40cd38e7fa0e
davidswygart <davidswygart@gmail.com>
2024-10-14 17:56:19 -0400

    echo message complete

--- backupDataToOneDrive.sh
+++ backupDataToOneDrive.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Uploading the piData folder to David's Onerive"
-xfce4-terminal --hold --command='rclone copy /home/server/Desktop/piData OneDrive_David:/piData_backup --metadata --human-readable -v'
+xfce4-terminal --hold -x bash -c 'rclone copy /home/server/Desktop/piData OneDrive_David:/piData_backup --metadata --human-readable -v ; echo transfer complete'
